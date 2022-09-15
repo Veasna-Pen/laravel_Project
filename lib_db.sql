25f60394a786921256d2623f710d442f72c7d9ff
Veasna <Veasna3d@gmail.com>
2022-09-15 16:10:24 +0700

    Add Pagination

---
+++ lib_db.sql
@@ -0,0 +1,178 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.18 
+MySQL - 5.5.5-10.4.10-MariaDB : Database - lib_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lib_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `lib_db`;
+
+/*Table structure for table `tbl_author` */
+
+DROP TABLE IF EXISTS `tbl_author`;
+
+CREATE TABLE `tbl_author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_name` varchar(255) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_author` */
+
+insert  into `tbl_author`(`id`,`author_name`,`create_date`) values (7,'errr','2022-09-14 21:37:16');
+
+/*Table structure for table `tbl_book` */
+
+DROP TABLE IF EXISTS `tbl_book`;
+
+CREATE TABLE `tbl_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_title` varchar(255) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `authorId` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_book` */
+
+insert  into `tbl_book`(`id`,`book_title`,`categoryId`,`authorId`,`status`,`create_date`) values (5,'Horo',4,6,6,'2022-09-07 22:36:57'),(6,'qww',4,7,7,'2022-09-14 21:37:46');
+
+/*Table structure for table `tbl_brrow` */
+
+DROP TABLE IF EXISTS `tbl_brrow`;
+
+CREATE TABLE `tbl_brrow` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `borrow_date` datetime NOT NULL,
+  `return_date` datetime NOT NULL,
+  `status` int(11) NOT NULL,
+  `remark` varchar(255) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_brrow` */
+
+/*Table structure for table `tbl_category` */
+
+DROP TABLE IF EXISTS `tbl_category`;
+
+CREATE TABLE `tbl_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(255) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_category` */
+
+insert  into `tbl_category`(`id`,`category_name`,`create_date`) values (4,'enchy','2022-09-07 22:16:46');
+
+/*Table structure for table `tbl_class` */
+
+DROP TABLE IF EXISTS `tbl_class`;
+
+CREATE TABLE `tbl_class` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(220) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_class` */
+
+/*Table structure for table `tbl_status` */
+
+DROP TABLE IF EXISTS `tbl_status`;
+
+CREATE TABLE `tbl_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status_name` varchar(250) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_status` */
+
+insert  into `tbl_status`(`id`,`status_name`,`create_date`) values (7,'gggg','2022-09-14 21:37:31');
+
+/*Table structure for table `tbl_student` */
+
+DROP TABLE IF EXISTS `tbl_student`;
+
+CREATE TABLE `tbl_student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `studentId` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_student` */
+
+/*Table structure for table `tbl_user` */
+
+DROP TABLE IF EXISTS `tbl_user`;
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `user_type` varchar(255) NOT NULL,
+  `user_ip` varchar(255) NOT NULL,
+  `verify_password` int(11) NOT NULL,
+  `time_login` text NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_user` */
+
+/*Table structure for table `vbook` */
+
+DROP TABLE IF EXISTS `vbook`;
+
+/*!50001 DROP VIEW IF EXISTS `vbook` */;
+/*!50001 DROP TABLE IF EXISTS `vbook` */;
+
+/*!50001 CREATE TABLE  `vbook`(
+ `id` int(11) ,
+ `book_title` varchar(255) ,
+ `category_name` varchar(255) ,
+ `author_name` varchar(255) ,
+ `status_name` varchar(250) ,
+ `create_date` timestamp 
+)*/;
+
+/*View structure for view vbook */
+
+/*!50001 DROP TABLE IF EXISTS `vbook` */;
+/*!50001 DROP VIEW IF EXISTS `vbook` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vbook` AS select `tbl_book`.`id` AS `id`,`tbl_book`.`book_title` AS `book_title`,`tbl_category`.`category_name` AS `category_name`,`tbl_author`.`author_name` AS `author_name`,`tbl_status`.`status_name` AS `status_name`,`tbl_book`.`create_date` AS `create_date` from (((`tbl_book` join `tbl_category` on(`tbl_book`.`categoryId` = `tbl_category`.`id`)) join `tbl_author` on(`tbl_book`.`authorId` = `tbl_author`.`id`)) join `tbl_status` on(`tbl_book`.`status` = `tbl_status`.`id`)) */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- resources/js/Pages/Communities/Index.vue
+++ resources/js/Pages/Communities/Index.vue
@@ -1,4 +1,5 @@
 <template>
+
     <Head title="All Communities" />
 
     <BreezeAuthenticatedLayout>
@@ -13,111 +14,77 @@
                 <div class="px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-end">
                         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                            <Link
-                                :href="route('communities.create')"
-                                class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                            >
-                                Add Community</Link
-                            >
+                            <Link :href="route('communities.create')"
+                                class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                            Add Community</Link>
                         </div>
                     </div>
                     <div class="mt-8 flex flex-col">
-                        <div
-                            class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8"
-                        >
-                            <div
-                                class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
-                            >
-                                <div
-                                    class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg"
-                                >
-                                    <table
-                                        class="min-w-full divide-y divide-gray-300"
-                                    >
+                        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                                <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-300">
                                         <thead class="bg-gray-50">
                                             <tr>
-                                                <th
-                                                    scope="col"
-                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                                                >
+                                                <th scope="col"
+                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
                                                     Name
                                                 </th>
-                                                <th
-                                                    scope="col"
-                                                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                                >
+                                                <th scope="col"
+                                                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                                                     Slug
                                                 </th>
-                                                <th
-                                                    scope="col"
-                                                    class="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                                                >
-                                                    <span class="sr-only"
-                                                        >Edit</span
-                                                    >
+                                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                                    <span class="sr-only">Edit</span>
                                                 </th>
                                             </tr>
                                         </thead>
-                                        <tbody
-                                            class="divide-y divide-gray-200 bg-white"
-                                        >
-                                            <tr
-                                                v-for="community in communities"
-                                                :key="community.id"
-                                            >
+                                        <tbody class="divide-y divide-gray-200 bg-white">
+                                            <tr v-for="community in communities.data" :key="community.id">
                                                 <td
-                                                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
-                                                >
+                                                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                                                     {{ community.name }}
                                                 </td>
-                                                <td
-                                                    class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"
-                                                >
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                                     {{ community.slug }}
                                                 </td>
                                                 <td
-                                                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
-                                                >
-                                                    <Link
-                                                        :href="
-                                                            route(
-                                                                'communities.edit',
-                                                                community.id
-                                                            )
-                                                        "
-                                                        class="text-indigo-600 hover:text-indigo-900"
-                                                        ><svg
-                                                            xmlns="http://www.w3.org/2000/svg"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            stroke-width="1.5"
-                                                            stroke="currentColor"
-                                                            class="w-6 h-6"
-                                                        >
-                                                            <path
-                                                                stroke-linecap="round"
-                                                                stroke-linejoin="round"
-                                                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                                                            />
-                                                        </svg>
+                                                    class="flex whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                    <Link :href="
+                                                        route(
+                                                            'communities.edit',
+                                                            community.id
+                                                        )
+                                                    " class="text-indigo-600 hover:text-indigo-900 mr-4"><svg
+                                                        xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                    </svg>
                                                     </Link>
-                                                    <Link
-                                                        :href="
-                                                            route(
-                                                                'communities.destroy',
-                                                                community.id
-                                                            )
-                                                        "
-                                                        class="text-red-600 hover:text-red-900"
-                                                        method="delete"
-                                                        as="button"
-                                                        type="button"
-                                                        ><svg class="h-8 w-8 text-red-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z" />  <line x1="18" y1="9" x2="12" y2="15" />  <line x1="12" y1="9" x2="18" y2="15" /></svg>
+                                                    <Link :href="
+                                                        route(
+                                                            'communities.destroy',
+                                                            community.id
+                                                        )
+                                                    " class="text-red-600 hover:text-red-900" method="delete"
+                                                        as="button" type="button"><svg
+                                                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                                        fill="currentColor" class="w-6 h-6">
+                                                        <path fill-rule="evenodd"
+                                                            d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+                                                            clip-rule="evenodd" />
+                                                    </svg>
+
                                                     </Link>
                                                 </td>
                                             </tr>
                                         </tbody>
                                     </table>
+                                    <div class="m-2 p-2">
+                                        <Pagination :links="communities.links" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -131,6 +98,7 @@
 <script setup>
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
 import { Head, Link } from "@inertiajs/inertia-vue3";
+import Pagination from "../../Components/Pagination.vue";
 
 defineProps({
     communities: Object,
